79b381f53a7896e630ffa3624eaadcf4ae436f39
Gabrie1Garcia <90660755+Gabrie1Garcia@users.noreply.github.com>
2022-05-05 18:59:51 -0300

    add

---
+++ script/etapas.js
@@ -0,0 +1,50 @@
+let = etapas = [
+      {
+      titulo: 'VEREADOR(A)',
+      numeros: 5,
+      candidatos: [
+          {
+              numero: '38111',
+              nome: 'Janaina da Silva',
+              partido: 'ABC', 
+              fotos: [
+                  {url:'candidata_1.jpeg', legenda: 'Vereador(a)'}
+              ]
+          },
+          {
+            numero: '48222',
+            nome: 'José do Salve',
+            partido: 'DEF', 
+            fotos: [
+                {url: 'candidato_4.jpeg', legenda: 'Vereador(a)'}   
+          ]    
+      },
+    ]
+  },
+  {
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+        {
+            numero: '99',
+            nome: 'João Meia Boca',
+            partido: 'ABC',
+            vice: 'Maria da Glória',
+            fotos: [
+                {url: 'candidato_1.jpeg', legenda: 'Prefeito'},
+                {url: 'candidata_2.jpeg', legenda: 'Vice-Prefeito', small: true}
+            ]
+        },
+        {
+            numero: '84',
+            nome: 'Maria Sorriso',
+            partido: 'DEF',
+            vice: 'ANA PRADO',
+            fotos: [
+                {url: 'candidata_3.jpeg', legenda: 'Prefeito'},
+                {url: 'candidata_4.jpeg', legenda: 'Vice-Prefeito', small: true}
+            ]  
+        },
+     ]
+   }
+];
